b3e0dc4e15b009bcd894f85b70856950c7df42da
jimfrenette <jimfrenette@yahoo.com>
2020-01-28 20:12:03 -0500

    js use api stub data example

--- src/aem-dev-myproject/ui.apps/src/main/content/jcr_root/apps/myproject/components/content/products/data/products.js
+++ src/aem-dev-myproject/ui.apps/src/main/content/jcr_root/apps/myproject/components/content/products/data/products.js
@@ -1,34 +1,151 @@
 use(function() {
 
-let products = [];
+  let products = [];
 
-products = [
+  products = [
     {
-      "_id": {
-        "$oid": "5968dd23fc13ae04d9000001"
-      },
-      "product_name": "sildenafil citrate",
-      "supplier": "Wisozk Inc",
-      "quantity": 261,
-      "unit_cost": "$10.47"
+      "id": "0001",
+      "type": "donut",
+      "name": "Cake",
+      "ppu": 0.55,
+      "batters":
+        {
+          "batter":
+            [
+              { "id": "1001", "type": "Regular" },
+              { "id": "1002", "type": "Chocolate" },
+              { "id": "1003", "type": "Blueberry" },
+              { "id": "1004", "type": "Devil's Food" }
+            ]
+        },
+      "topping":
+        [
+          { "id": "5001", "type": "None" },
+          { "id": "5002", "type": "Glazed" },
+          { "id": "5005", "type": "Sugar" },
+          { "id": "5007", "type": "Powdered Sugar" },
+          { "id": "5006", "type": "Chocolate with Sprinkles" },
+          { "id": "5003", "type": "Chocolate" },
+          { "id": "5004", "type": "Maple" }
+        ]
     },
     {
-      "_id": {
-        "$oid": "5968dd23fc13ae04d9000002"
-      },
-      "product_name": "Mountain Juniperus ashei",
-      "supplier": "Keebler-Hilpert",
-      "quantity": 292,
-      "unit_cost": "$8.74"
+      "id": "0002",
+      "type": "donut",
+      "name": "Raised",
+      "ppu": 0.55,
+      "batters":
+        {
+          "batter":
+            [
+              { "id": "1001", "type": "Regular" }
+            ]
+        },
+      "topping":
+        [
+          { "id": "5001", "type": "None" },
+          { "id": "5002", "type": "Glazed" },
+          { "id": "5005", "type": "Sugar" },
+          { "id": "5003", "type": "Chocolate" },
+          { "id": "5004", "type": "Maple" }
+        ]
+    },
+
+    {
+      "id": "0003",
+      "type": "donut",
+      "name": "Old Fashioned",
+      "ppu": 0.55,
+      "batters":
+        {
+          "batter":
+            [
+              { "id": "1001", "type": "Regular" },
+              { "id": "1002", "type": "Chocolate" }
+            ]
+        },
+      "topping":
+        [
+          { "id": "5001", "type": "None" },
+          { "id": "5002", "type": "Glazed" },
+          { "id": "5003", "type": "Chocolate" },
+          { "id": "5004", "type": "Maple" }
+        ]
     },
     {
-      "_id": {
-        "$oid": "5968dd23fc13ae04d9000003"
-      },
-      "product_name": "Dextromathorphan HBr",
-      "supplier": "Schmitt-Weissnat",
-      "quantity": 211,
-      "unit_cost": "$20.53"
+      "id": "0004",
+      "type": "bar",
+      "name": "Bar",
+      "ppu": 0.75,
+      "batters":
+        {
+          "batter":
+            [
+              { "id": "1001", "type": "Regular" },
+            ]
+        },
+      "topping":
+        [
+          { "id": "5003", "type": "Chocolate" },
+          { "id": "5004", "type": "Maple" }
+        ],
+      "fillings":
+        {
+          "filling":
+            [
+              { "id": "7001", "name": "None", "addcost": 0 },
+              { "id": "7002", "name": "Custard", "addcost": 0.25 },
+              { "id": "7003", "name": "Whipped Cream", "addcost": 0.25 }
+            ]
+        }
+    },
+    {
+      "id": "0005",
+      "type": "twist",
+      "name": "Twist",
+      "ppu": 0.65,
+      "batters":
+        {
+          "batter":
+            [
+              { "id": "1001", "type": "Regular" },
+            ]
+        },
+      "topping":
+        [
+          { "id": "5002", "type": "Glazed" },
+          { "id": "5005", "type": "Sugar" },
+        ]
+    },
+    {
+      "id": "0006",
+      "type": "filled",
+      "name": "Filled",
+      "ppu": 0.75,
+      "batters":
+        {
+          "batter":
+            [
+              { "id": "1001", "type": "Regular" },
+            ]
+        },
+      "topping":
+        [
+          { "id": "5002", "type": "Glazed" },
+          { "id": "5007", "type": "Powdered Sugar" },
+          { "id": "5003", "type": "Chocolate" },
+          { "id": "5004", "type": "Maple" }
+        ],
+      "fillings":
+        {
+          "filling":
+            [
+              { "id": "7002", "name": "Custard", "addcost": 0 },
+              { "id": "7003", "name": "Whipped Cream", "addcost": 0 },
+              { "id": "7004", "name": "Strawberry Jelly", "addcost": 0 },
+              { "id": "7005", "name": "Rasberry Jelly", "addcost": 0 }
+            ]
+        }
     }
   ]
 
