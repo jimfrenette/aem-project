c9ae17e5384adc09640aad28a3cb05924e482fd8
jimfrenette <jimfrenette@yahoo.com>
2020-11-02 08:52:23 -0500

    dialog: cascade select to populate select 2 items on change

--- src/myproject/ui.apps/src/main/content/jcr_root/apps/myproject/components/helloworld/clientlib-edit/dialog.js
+++ src/myproject/ui.apps/src/main/content/jcr_root/apps/myproject/components/helloworld/clientlib-edit/dialog.js
@@ -33,29 +33,67 @@
             return content;
         }
 
+        function getElementIndex(node) {
+            var index = 0;
+            while ( (node = node.previousElementSibling) ) {
+                index++;
+            }
+            return index;
+        }
+
+        function getModels(make) {
+            for (var i = 0, len = carsdata.length; i < len; ++i) {
+                if (make == carsdata[i].make) {
+                    return carsdata[i].models;
+                }
+            }
+        }
+
+        function populateModel(select, make) {
+            var models = getModels(make);
+            Coral.commons.ready(select, function (component) {
+                component.items.clear();
+                models.forEach(function (model) {
+                    var option = document.createElement('coral-select-item');
+                    option.textContent = model;
+                    component.items.add(option);
+                });
+            });
+        }
+
         /**
          * @param {*} select (element)
          * @param {*} content (jcr:content)
          */
         function populateItem(select, key, content) {
             for (var i = 0, len = carsdata.length; i < len; ++i) {
-                var option = document.createElement('coral-select-item');
                 if (key == 'make') {
+                    var option = document.createElement('coral-select-item');
                     option.textContent = carsdata[i].make
-                    option.value = carsdata[i].make;
-                } else {
+                    select.appendChild(option);
+                } else if (content.make == carsdata[i].make) { // models
+                    carsdata[i].models.forEach(function (model) {
+                        var option = document.createElement('coral-select-item');
+                        option.textContent = model;
+                        select.appendChild(option);
+                    });
                 }
-                select.appendChild(option);
             }
 
             Coral.commons.ready(select, function (component) {
                 if (key == 'make') {
                     if (content) {
-                        component.value = content.make; }
+                        component.value = content.make;
+                    }
                     component.addEventListener('change', function(evt) {
-                        // todo: cascade selection
+                        // cascade selection
+                        var item = select.closest('[role=listitem]');
+                        var i = getElementIndex(item);
+                        var select2 = item.querySelector(`coral-select[name="./cars/item${i}/./model"]`);
+                        populateModel(select2, component.value);
                     });
-                } else {
+                } else if (content) {
+                    component.value = content.model;
                 }
             });
         }
@@ -87,7 +125,7 @@
                 // give coral a sec to inject fields
                 setTimeout(function(){
                     populateItems();
-                }, 1000);
+                }, 500);
             }
 
             carsfield.add.addEventListener('click', function() {
@@ -97,7 +135,7 @@
                     var index = carsfield.items.length - 1,
                         select = carsfield.items[index].querySelector(`coral-select[name="./cars/item${index}/./make"]`);
                     populateItem(select, 'make', null);
-                }, 1000);
+                }, 500);
             });
         }
 
